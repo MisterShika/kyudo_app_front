dbc00365996afdd39ff37182d822d8f40f8d5f00
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-05-14 19:49:46 +0900

    Frontend is now capable of making simple request to backend.

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,22 +1,36 @@
 import GoogleProvider from 'next-auth/providers/google';
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+async function accountExists () {
+    console.log("accountexists fired");
+}
 
 export const options = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ account, profile }) {
-      const allowedDomains = ['gmail.com'];
-      const email = profile?.email || '';
-      const domain = email.split('@')[1];
-
-      if (account?.provider === 'google' && allowedDomains.includes(domain)) {
-        return true;
-      }
-      return false;
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    session: {
+        strategy: 'jwt'
     },
-  },
+    callbacks: {
+        async signIn({ account, profile }) {
+            const email = profile?.email || '';
+            const domain = email.split('@')[1];
+            if (account?.provider === 'google' && domain === 'gmail.com') {
+
+                try{
+                    const res = await fetch(`${apiBaseUrl}/users`);
+                    console.log(res);
+                }catch(error){
+                    console.error('Failed to reach Express backend:', error);
+                }
+
+                return true;
+            }
+            return false;
+        },
+    },
 };
