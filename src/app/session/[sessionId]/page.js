528d649d5d52667a46ea2953eb0bad19d7977ff4
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-05-25 17:34:40 +0900

    Session now pulling data from database.

--- src/app/session/[sessionId]/page.js
+++ src/app/session/[sessionId]/page.js
@@ -1,16 +1,30 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { use, useState } from 'react';
+import { use, useState, useEffect } from 'react';
 
 
 export default function SingleSession ({ params }) {
-
     const { sessionId } = use(params);
+    const [sessionData, setSessionData] = useState(null);
 
-    return (
-        <div>
-            <h1>Session ID: {sessionId}</h1>
-        </div>
-    );
+    useEffect(() => {
+        fetch(`http://localhost:3000/sessions/getSession/${sessionId}`)
+            .then(res => res.json())
+            .then(setSessionData);
+    }, []);
+
+    if(sessionData == null){
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    }else{
+        return (
+            <div>
+                {JSON.stringify(sessionData, null, 2)}
+            </div>
+        );
+    }
 }
