69d254514fd6d6962899179a561489b6a70142f4
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-05-26 15:56:42 +0900

    Header and footer files added.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import SyncUserAfterLogin from "../components/syncuserafterlogin";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +12,9 @@ export default function RootLayout({ children }) {
       <body>
         <SessionProvider>
           <SyncUserAfterLogin />
+          <Header />
           {children}
+          <Footer />
         </SessionProvider>
       </body>
     </html>
