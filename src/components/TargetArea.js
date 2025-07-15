b0d548adb5d9da24819fea06b64adee460e6d468
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-07-15 20:14:15 +0900

    Target area now present. Issues present at low width devices.

--- src/components/TargetArea.js
+++ src/components/TargetArea.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 export default function TargetArea() {
     const [target, setTarget] = useState([]);
@@ -12,8 +13,15 @@ export default function TargetArea() {
                     Information
                 </div>
                 <div className="target-container flex-grow flex items-center justify-center">
-                    <div className="target-area aspect-[18/10] h-full max-w-[100%] bg-blue-300">
-                        
+                    <div className="target-area aspect-[18/10] h-full bg-blue-300">
+                        <div className="relative w-[28%] h-[50%]">
+                            <Image
+                            src="/images/kyudoTarget.png"
+                            alt="Kyudo Target"
+                            fill
+                            className="object-contain"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="confirmation-rect flex items-center justify-center h-8 bg-purple-200">
