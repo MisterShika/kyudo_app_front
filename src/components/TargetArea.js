31206391d168e9f132bc01d2d95bed73a5f4ef56
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-07-16 19:49:17 +0900

    Target now in appropriate place. Clicks now registering.

--- src/components/TargetArea.js
+++ src/components/TargetArea.js
@@ -1,20 +1,30 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Image from 'next/image';
 
 export default function TargetArea() {
     const [target, setTarget] = useState([]);
+    const targetRef = useRef(null);
+
+    const targetClick = (e) => {
+    const rect = targetRef.current.getBoundingClientRect();
+
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    console.log(`Clicked at X: ${x.toFixed(2)}%, Y: ${y.toFixed(2)}%`);
+    }
 
     return (
         <div className="target-box h-full">
-            <div className="top-target flex flex-col h-auto sm:h-[52%] min-h-[264px]">
+            <div className="top-target flex flex-col h-auto sm:h-[52%]">
                 <div className="notice-area flex items-center justify-center h-8 bg-green-200">
                     Information
                 </div>
                 <div className="target-container flex-grow flex items-center justify-center">
-                    <div className="target-area aspect-[9/5] w-full h-auto sm:w-auto sm:h-full bg-blue-300">
-                        <div className="relative w-[28%] h-[50%]">
+                    <div ref={targetRef} onClick={targetClick} className="target-area relative flex items-end justify-center aspect-[9/5] w-full h-auto sm:w-auto sm:h-full bg-blue-300">
+                        <div className="relative w-[28%] h-[50%] mb-[5%]">
                             <Image
                             src="/images/kyudoTarget.png"
                             alt="Kyudo Target"
